80a576a483e2ad0a91ffd4cf8a5dc1f7c4ee788b
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-27 19:18:35 -0400

    Users table is now seededgit add .! It's working

--- react-multi-page-website/server/db/schema/01_users.sql
+++ react-multi-page-website/server/db/schema/01_users.sql
@@ -8,9 +8,6 @@ CREATE TABLE users (
   password VARCHAR(50) NOT NULL,
   date_of_birth DATE NOT NULL,
   address VARCHAR(255) NOT NULL,
-  latitude VARCHAR(255) NOT NULL,
-  longitude VARCHAR(255) NOT NULL,
-  gender_id INTEGER REFERENCES genders(id) ON DELETE CASCADE,
   about_me TEXT NOT NULL,
   starsign BOOLEAN DEFAULT false,
   vaccinated BOOLEAN NOT NULL
--- react-multi-page-website/server/routes/users.js
+++ react-multi-page-website/server/routes/users.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 
 module.exports = db => {
   router.get("/users", (request, response) => {
-    db.query(`SELECT first_name FROM users`).then(({ rows: users }) => {
+    db.query(`SELECT * FROM users`).then((results) => {
+      const users = results.rows;
       response.json(
-        users.reduce(
-          (previous, current) => ({ ...previous, [current.id]: current }),
-          {}
-        )
-      );
+        users)
+        // (
+        //   (previous, current) => ({ ...previous, [current.id]: current }),
+        //   {}
+        // ))
+      ;
     });
   });
 
